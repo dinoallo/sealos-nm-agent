d62d77f9d709209eb1614a81747db420d55bef0b
Yun Pan <dinoallo@netc.it>
2024-03-07 11:40:44 +0800

    upgrade: agent protoapi to date

--- proto_gen.go
+++ proto_gen.go
@@ -1,3 +1,3 @@
 package main
 
-//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative proto/agent/counter.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative proto/agent/traffic.proto
