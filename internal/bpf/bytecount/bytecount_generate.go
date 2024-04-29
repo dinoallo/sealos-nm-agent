a4050bd6859eefe6893668bd673b40126c0c12a1
Yun Pan <dinoallo@netc.it>
2024-04-29 14:27:34 +0800

    cleanup: remove old code

--- internal/bpf/bytecount/bytecount_generate.go
+++
@@ -1,4 +0,0 @@
-package bytecount
-
-// $BPF_CLANG and $BPF_CFLAGS are set by the Makefile.
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -type traffic_event_t -cc $BPF_CLANG -cflags $BPF_CFLAGS bytecount bytecount.c -- -I../headers
