b1be40d31813e958fe16fe50910a10e4f8aee7bc
Yun Pan <dinoallo@netc.it>
2024-07-24 23:11:44 +0800

    cleanup: remove unused bpf.c files

--- internal/bpf/traffic/traffic_generate.go
+++ internal/bpf/traffic/traffic_generate.go
@@ -1,6 +1,4 @@
 package traffic
 
 // $BPF_CLANG and $BPF_CFLAGS are set by the Makefile.
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS lxc_traffic lxc_traffic.c -- -I../headers
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS host_traffic host_traffic.c -- -I../headers
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS cep_traffic cep_traffic.c -- -I../headers
