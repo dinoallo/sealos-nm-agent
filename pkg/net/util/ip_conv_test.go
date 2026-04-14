23290e30a97ef8f5e03b527e06269f56b270644d
Yun Pan <dinoallo@netc.it>
2026-04-14 11:02:43 +0800

    fix: IP conversion test endianness

--- pkg/net/util/ip_conv_test.go
+++ pkg/net/util/ip_conv_test.go
@@ -6,10 +6,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TODO: this test is not correct...
 func TestIPConverting(t *testing.T) {
 	t.Run("ipv4 converting", func(t *testing.T) {
-		var ip uint32 = 3232236290
+		var ip uint32 = 0x0203a8c0
 		addr, ok := ToIP(ip, nil, 4)
 		if assert.Equal(t, true, ok) {
 			assert.Equal(t, "192.168.3.2", addr.String())
@@ -18,10 +17,10 @@ func TestIPConverting(t *testing.T) {
 	// v6Addr1 := "2001:db8:3333:4444:CCCC:DDDD:EEEE:FFFF"
 	t.Run("ipv6 converting 1", func(t *testing.T) {
 		var ip []uint32
-		ip = append(ip, 0x20010db8)
-		ip = append(ip, 0x33334444)
-		ip = append(ip, 0xccccdddd)
-		ip = append(ip, 0xeeeeffff)
+		ip = append(ip, 0xb80d0120)
+		ip = append(ip, 0x44443333)
+		ip = append(ip, 0xddddcccc)
+		ip = append(ip, 0xffffeeee)
 		addr, ok := ToIP(0, ip, 6)
 		if assert.Equal(t, true, ok) {
 			assert.Equal(t, "2001:db8:3333:4444:cccc:dddd:eeee:ffff", addr.String())
@@ -30,10 +29,10 @@ func TestIPConverting(t *testing.T) {
 	// v6Addr2 := "2001:db8:1::ab9:C0A8:102"
 	t.Run("ipv6 converting 2", func(t *testing.T) {
 		var ip []uint32
-		ip = append(ip, 0x20010db8)
-		ip = append(ip, 0x00010000)
-		ip = append(ip, 0x00000ab9)
-		ip = append(ip, 0xc0a80102)
+		ip = append(ip, 0xb80d0120)
+		ip = append(ip, 0x00000100)
+		ip = append(ip, 0xb90a0000)
+		ip = append(ip, 0x0201a8c0)
 		addr, ok := ToIP(0, ip, 6)
 		if assert.Equal(t, true, ok) {
 			assert.Equal(t, "2001:db8:1::ab9:c0a8:102", addr.String())
@@ -43,8 +42,8 @@ func TestIPConverting(t *testing.T) {
 		var ip []uint32
 		ip = append(ip, 0x00000000)
 		ip = append(ip, 0x00000000)
-		ip = append(ip, 0x0000ffff)
-		ip = append(ip, 0xc0a80302)
+		ip = append(ip, 0xffff0000)
+		ip = append(ip, 0x0203a8c0)
 		addr, ok := ToIP(0, ip, 6)
 		if assert.Equal(t, true, ok) {
 			assert.Equal(t, "::ffff:192.168.3.2", addr.String())
