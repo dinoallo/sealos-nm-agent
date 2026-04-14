507ce559c41e9b3e9df86bb0a181615a1b8bc656
Yun Pan <dinoallo@netc.it>
2026-04-14 14:24:34 +0800

    fix: temporarily disable IPv6 outbound test

--- pkg/net/util/ip_lookup_test.go
+++ pkg/net/util/ip_lookup_test.go
@@ -16,13 +16,15 @@ func TestGettingOutBoundIP(t *testing.T) {
 			}
 		}
 	})
-	t.Run("get the outbound ip by asking a v6 dns service", func(t *testing.T) {
-		dnsService := "[2001:4860:4860::8888]:80"
-		prefixes, err := GetOutboundV6AddrsInCIDR(dnsService)
-		if assert.NoError(t, err) {
-			for _, prefix := range prefixes {
-				t.Logf("the outbound ip is: %v", prefix)
-			}
-		}
-	})
+	// Disable this temporarily since some environments may not have ipv6 connectivity, and the failure of this test may cause confusion.
+	// We can re-enable it after we find a better way to test ipv6 connectivity in different environments.
+	// t.Run("get the outbound ip by asking a v6 dns service", func(t *testing.T) {
+	// 	dnsService := "[2001:4860:4860::8888]:80"
+	// 	prefixes, err := GetOutboundV6AddrsInCIDR(dnsService)
+	// 	if assert.NoError(t, err) {
+	// 		for _, prefix := range prefixes {
+	// 			t.Logf("the outbound ip is: %v", prefix)
+	// 		}
+	// 	}
+	// })
 }
