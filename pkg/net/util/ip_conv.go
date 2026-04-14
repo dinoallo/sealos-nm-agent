3365d4e8146f79b0efaf769bb1039ae686301a67
Yun Pan <dinoallo@netc.it>
2026-04-14 11:14:19 +0800

    feat: use host byte order for IP conversion
    Detect host endianness at runtime and use it for encoding
    Apply to IPv4/IPv6 uint32 words; add hostEndian helper
    Callers must pass integers in host byte order

--- pkg/net/util/ip_conv.go
+++ pkg/net/util/ip_conv.go
@@ -4,6 +4,7 @@ import (
 	"encoding/binary"
 	"net"
 	"net/netip"
+	"unsafe"
 )
 
 func ToIP(_v4Addr uint32, _v6Addr []uint32, t int) (netip.Addr, bool) {
@@ -15,18 +16,31 @@ func ToIP(_v4Addr uint32, _v6Addr []uint32, t int) (netip.Addr, bool) {
 	return netip.Addr{}, false
 }
 
-// TODO: use native endian
+func hostEndian() binary.ByteOrder {
+	var i uint16 = 0x0102
+	b := *(*[2]byte)(unsafe.Pointer(&i))
+	if b[0] == 0x02 {
+		return binary.LittleEndian
+	}
+	return binary.BigEndian
+}
+
+// toIPv4 converts a host-endian uint32 into an IP address.
+// Callers must pass the IPv4 integer in host byte order.
 func toIPv4(nn uint32) (netip.Addr, bool) {
 	ip := make([]byte, net.IPv4len)
-	binary.LittleEndian.PutUint32(ip, nn)
+	hostEndian().PutUint32(ip, nn)
 	return netip.AddrFromSlice(ip)
 }
 
+// toIPv6 converts four host-endian uint32 words into an IPv6 address.
+// Callers must pass each IPv6 word in host byte order.
 func toIPv6(nn []uint32) (netip.Addr, bool) {
 	var ip []byte
+	order := hostEndian()
 	for i := 0; i < 4; i++ {
 		_ip := make([]byte, net.IPv4len)
-		binary.LittleEndian.PutUint32(_ip, nn[i])
+		order.PutUint32(_ip, nn[i])
 		ip = append(ip, _ip...)
 	}
 	return netip.AddrFromSlice(ip)
